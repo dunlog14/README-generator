99573fd8edce0c1079be98ead0779fd8a2d7a653
Logan Dunaway <dunaway14@gmail.com>
2023-03-07 17:15:06 -0500

    added js and readme

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
-# Professional README Generator Starter Code
+# readme-generator
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+This app generates a README.md file using node.js. Not only will the app generate the file, but it will also prompt the user to answer each mandatory README question.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+Users can install the app by cloning the repo down through GitHub.
+
+## Usage
+
+Log "node index.js" in the terminal to begin. The questions will then start to generate in the user’s terminal.
+
+## License
+
+None.
+
+## Contributing
+
+N/A
+
+## Tests
+
+You can test the project by seeing if a README.md file generates after answering all prompted questions.
+
+## Links
+
+https://drive.google.com/drive/folders/1ExFcX2DzV3t5mt-ER7aC1WMduO7zsfBv
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at dunaway14@gmail.com. You can find more of my work at https://github.com/dunlog14.
--- index.js
+++ index.js
@@ -1,13 +1,100 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const axios = require('axios');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// README Q's
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Please enter a description of your project:',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Please enter installation instructions for your project:',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'Please enter usage information for your project:',
+      name: 'usage',
+    },
+    {
+      type: 'list',
+      message: 'Please choose a license for your project:',
+      name: 'license',
+      choices: ['MIT', 'GNU GPLv3', 'Apache License 2.0', 'ISC', 'Unlicense'],
+    },
+    {
+      type: 'input',
+      message: 'Please enter contribution guidelines for your project:',
+      name: 'contributing',
+    },
+    {
+      type: 'input',
+      message: 'Please enter test instructions for your project:',
+      name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your GitHub repo link:',
+        name: 'Links',
+      },
+    {
+      type: 'input',
+      message: 'Please enter your GitHub username:',
+      name: 'githubUsername',
+    },
+    {
+      type: 'input',
+      message: 'Please enter your email address:',
+      name: 'email',
+    },
+  ])
+  .then((answers) => {
+    // Generate the README
+    const readmeContent = `# ${answers.title}
+${answers.description}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+${answers.installation}
+## Usage
+${answers.usage}
+## License
+This project is licensed under the ${answers.license} license. [![License: ${answers.license}](https://img.shields.io/badge/License-${answers.license}-yellow.svg)](https://opensource.org/licenses/${answers.license})
+## Contributing
+${answers.contributing}
+## Tests
+${answers.tests}
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at ${
+      answers.email
+    }. You can find more of my work at https://github.com/${
+      answers.githubUsername
+    }.`;
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+    // Write the README file to disk
+    return writeFileAsync('README.md', readmeContent);
+  })
+  .then(() => {
+    console.log('README.md file generated!');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+  
